1524d0dbc81a384da5fe1a5f8ce197a94843f09d
Akash Panwar <akashpanwar9976@gmail.com>
2022-10-01 14:57:02 +0530

    Redirecting

--- src/pages/welcome.js
+++ src/pages/welcome.js
@@ -1,5 +1,14 @@
+import { Route } from 'react-router-dom';
+
 const Welcome = () => {
-  return <h1>The welcome page</h1>;
+  return (
+    <section>
+      <h1>The welcome page</h1>
+      <Route path='/welcome/new-user'>
+        <p>Welcome, new user!😊</p>
+      </Route>
+    </section>
+  );
 };
 
 export default Welcome;
